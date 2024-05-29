febedb8bbe47ccf4b12803ddb235071e8c694ee6
chihang24 <hochihang24@gmail.com>
2024-05-30 01:12:59 +0800

    MERN

--- MERN-lowest-level/backend/config.js
+++ MERN-lowest-level/backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 8000;
 
-export const mongoDBURL = '';
+export const mongoDBURL = `mongodb+srv://${process.env.MONGO_USER_NAME}:${process.env.MONGO_USER_PASSWORD}@hellomongo.ztlgbwz.mongodb.net/?retryWrites=true&w=majority&appName=HelloMongo`
